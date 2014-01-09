f2a17d9a17289cf10cba962321e8029e6c7caf24
ShabiniRajadas <shabiniraja90@gmail.com>
2014-01-09 15:09:01 +0530

    Riak controller change

--- app/controllers/name_controller.rb
+++ app/controllers/name_controller.rb
@@ -6,21 +6,4 @@ class NameController < ApplicationController
 		@keys = client.list_keys('orders')
 	end
 
-	def create
-		@name = Name.new(name_params)
-		if @name.save
-			flash[:notice] = "User is created Successfully."
-		else
-			render :action => :new
-		end
-	end
-
-	def show
-	end
-
-	private
-
-	def name_params
-		params.require(:name).permit(:name, :order, :number)
-	end
 end
